391b32391becdccaeca59b8bb0d4c65fa46c6f6e
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-05 20:28:00 +0000

    feat: successful db connection

--- index.ts
+++ index.ts
@@ -1,15 +1,47 @@
 import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import * as mongoDB from "mongodb";
 
 dotenv.config();
 
 const app: Express = express();
+app.use(cors());
+app.use(require("body-parser").json());
+
+export const collections: { stories?: mongoDB.Collection } = {}
+
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server for forodain');
-});
+const uri = "mongodb://192.168.5.58:27017/forodainDB";
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+export async function connectToDatabase () {
+  dotenv.config();
+
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(process.env.DB_CONN_STRING as string);
+          
+  await client.connect();
+      
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+ 
+  const storiesCollection: mongoDB.Collection = db.collection(process.env.STORIES_COLLECTION_NAME as string);
+
+collections.stories = storiesCollection;
+     
+       console.log(`Successfully connected to database: ${db.databaseName} and collection: ${storiesCollection.collectionName}`);
+};
+
+
+
+
+
+  app.get('/', (req: Request, res: Response) => {
+    connectToDatabase();
+    res.send('Express + TypeScript Server for forodain');
+    
+  });
+
+
+  app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+  });
